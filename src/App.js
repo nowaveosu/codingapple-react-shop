8d6789dd037071224ea04a6ef04cfacbb46f1579
nowaveosu <nowaveosu@gmail.com>
2023-05-07 16:32:59 +0900

    data binding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import './App.css';
-import { Button, Nav, Navbar, Container} from 'react-bootstrap';
-import bg from './img/bg.png';
-
 // * bootstrap
 // 상단바, 모달, 버튼 등 맘대로 갖다쓸수있음
 // index.html에 <link> 추가하거나 app.js에 import하면됨
@@ -14,8 +10,22 @@ import bg from './img/bg.png';
 // public폴더 이미지 사용할땐 그냥 src="/logo.png" 이렇게 쓰면 편함
 // <img src={process.env.PUBLIC_URL + '/logo192.png'} /> 서브경로 문제를 위해 public 쓰면 이렇게쓰는게 더 좋음
 
+//* 파일간 변수공유
+// data.js에서 export default 변수명 해서 내보내서
+// App.js에서 import 아무이름 from './data.js'해서 씀
+// export 여러개하려면 export {a,b}, import {a,b} from './data
+
+
+import './App.css';
+import { Button, Nav, Navbar, Container} from 'react-bootstrap';
+import bg from './img/bg.png';
+import { useState } from 'react';
+import data from './data';
 
 function App() {
+
+  let [shoes] = useState(data)
+  console.log(shoes);
   return (
     <div className="App">
 
@@ -34,18 +44,18 @@ function App() {
         <div className='row'>
           <div className='col-md-4'>
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
           </div>
           <div className='col-md-4'>
             <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
-              <h4>상품명</h4>
-              <p>상품설명</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
           </div>
           <div className='col-md-4'>
             <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
-              <h4>상품명</h4>
-              <p>상품설명</p>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
           </div>
         </div>
       </div>
