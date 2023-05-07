58fab014582ef8ffc0cb04a6e0158a97b63af8fd
nowaveosu <nowaveosu@gmail.com>
2023-05-07 14:26:48 +0900

    Navbar 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Nav, Navbar, Container} from 'react-bootstrap';
+
+// * bootstrap
+// 상단바, 모달, 버튼 등 맘대로 갖다쓸수있음
+// index.html에 <link> 추가하거나 app.js에 import하면됨
+// 복붙하면 되고, className으로 커스터마이징 가능함
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">밤무리상점</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
