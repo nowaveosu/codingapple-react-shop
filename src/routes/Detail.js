742e9cac56b81b818ad9785373387402764daf42
nowaveosu <nowaveosu@gmail.com>
2023-05-11 21:43:36 +0900

    useEffect 안에 java

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -12,8 +12,7 @@ const YellowBtn = styled.button`
 function Detail (props) {
 
     useEffect(() => {
-
-        let a = setTimeout(()=>{setalert(false)},2000)
+        let a = setTimeout(()=>{seta1lert(false)},2000)
         console.log(2);
         return() => {
             console.log(1)
@@ -21,12 +20,20 @@ function Detail (props) {
         }
     },[])
     
-    let [count, setCount] = useState(0)
-    let [alert, setalert] = useState(true)
 
+    let [count, setCount] = useState(0)
+    let [a1lert, seta1lert] = useState(true)
+    let [warn, setWarn] = useState(false)
+    let [gap, setGap] = useState();
+    useEffect(()=>{
+        if(isNaN(gap)){
+            alert("그러지마셈")
+        }
+    },[gap])
+    
     let {id} = useParams();
     let 찾은상품 = props.shoes.find(x => x.id == id);
-
+    console.log(typeof(gap));
     console.log(id);
     return (
         <div className="container">
@@ -43,6 +50,15 @@ function Detail (props) {
                 <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
                 </div>
                 <div className="col-md-6">
+                {
+                    
+                    isNaN(gap) 
+                    ? <div>숫자만 입력하세요</div>
+                    : null
+                }
+                <input onChange={(e) => {
+                    setGap(e.target.value);
+                }}></input>
                 <h4 className="pt-5">{props.shoes[id].title}</h4>
                 <p>{props.shoes[id].content}</p>
                 <p>{props.shoes[id].price}원</p>
