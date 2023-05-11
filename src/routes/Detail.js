87fe70d08b5b3bffe6b69d03bf4ca959bd044c5b
nowaveosu <nowaveosu@gmail.com>
2023-05-11 21:12:33 +0900

    useEffect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,12 +1,43 @@
-import React from "react";
+/* eslint-disable */ 
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { styled } from "styled-components";
+
+const YellowBtn = styled.button`
+    background : ${props => props.bg};
+    color : ${props => props.bg === "blue" ? "white" : "black"};
+    padding : 10px;
+`
 
 function Detail (props) {
 
+    useEffect(() => {
+
+        let a = setTimeout(()=>{setalert(false)},2000)
+        console.log(2);
+        return() => {
+            console.log(1)
+            clearTimeout(a)
+        }
+    },[])
+    
+    let [count, setCount] = useState(0)
+    let [alert, setalert] = useState(true)
+
     let {id} = useParams();
+    let 찾은상품 = props.shoes.find(x => x.id == id);
+
     console.log(id);
     return (
         <div className="container">
+            {
+                alert === true
+                ? (
+                    <div className="alert alert-warning">2초이내 구매시 할인</div>
+                )
+                : null
+            }
+
             <div className="row">
                 <div className="col-md-6">
                 <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
